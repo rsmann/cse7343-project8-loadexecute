8138c68d4580e714803e2d670812a95009fbd052
Scott Mann <ssmann@smu.edu>
2013-11-26 06:33:42 -0600

    Step 5 working. Step 6 calls and returns, but no output.

--- shell.c
+++ shell.c
@@ -1,9 +1,9 @@
-// Scott Mann - CSE 7343 - 10/20/2013
-// A simple Unix shell.
+// Scott Mann - CSE 7343 - 11/26/2013
+// A simple shell to go with the kernel.
 
 // Function Prototypes
 void displayError();
-int isType(char* input);
+int matches(char*, char*);
 
 // Main processing loop for the shell
 int main() {
@@ -14,11 +14,14 @@ int main() {
 	int partIndex = 0;
 	int offset = 0;
 
-	interrupt(0x21, 0, "Shell Version 0.1.234\r\n\0", 0, 0);
+	interrupt(0x21, 0, "Shell Version 0.2.234\r\n\0", 0, 0);
 
 	// Begin the command processing loop
 	while(1)
 	{
+		partIndex = 0;
+		offset = 0;
+
 		// Capture command line input
 		interrupt(0x21, 0, "Shell> ", 0, 0);
 		interrupt(0x21, 1, input, 0, 0);
@@ -26,15 +29,15 @@ int main() {
 
 		for (i = 0; i < 255; i++)
 		{
-			if (input[i] == ' ')
+			if (input[i] == '\0' || input[i] == '\r')
+			{
+				break;
+			}
+			else if (input[i] == ' ')
 			{
 				partIndex++;
 				offset = 0;
 				continue;
-			}
-			else if (input[i] == '\0')
-			{
-				break;
 			}
 
 			part[partIndex][offset] = input[i];
@@ -43,16 +46,18 @@ int main() {
 
 		if (matches("execute", part[0]) == 1)
 		{
+			interrupt(0x21, 0, "Execute!\r\n\0", 0, 0);
 			interrupt(0x21, 0x6, part[1], 0, 0);
 		}
 		else if (matches("type", part[0]) == 1)
 		{
-			//interrupt(0x21, 0, "Type!\r\n\0", 0, 0);
+			interrupt(0x21, 0, "Type!\r\n\0", 0, 0);
 			interrupt(0x21, 0x3, part[1], buffer, 0);
 			interrupt(0x21, 0x0, buffer, 0, 0);
 		}
 		else
 		{
+			interrupt(0x21, 0, "Error!\r\n\0", 0, 0);
 			displayError();
 		}
 
@@ -71,11 +76,11 @@ int matches(char* s1, char* s2)
 
 	if (size2 >= size1)
 	{
-		maxSize = size2;
+		maxSize = size1;
 	}
 	else
 	{
-		maxSize = size1;
+		maxSize = size2;
 	}
 
 	interrupt(0x21, 0, s1, 0, 0);
@@ -102,65 +107,3 @@ void displayError()
 {
 	interrupt(0x21, 0, "Bad command!\r\n\0", 0, 0);
 }
-
-// Execute the command passed in
-//void execute(char* command)
-//{
-//	char args[1][1];
-//	args[0][0] = '\0';
-//
-//	int pid = fork();
-//
-//	if (pid == 0)
-//	{
-//		printf("Executing...\n");
-//		execvp(command, args);
-//	}
-//	else
-//	{
-//		wait(pid);
-//		return;
-//	}
-//}
-
-// Is the command "exit"
-//bool isExit(char* command)
-//{
-//	return !strcmp(command, "exit");
-//}
-
-// Is the command "type"
-//bool isType(char* command)
-//{
-//	return !strcmp(command, "type");
-//}
-
-// Output the file to the screen byte by byte
-//void type(char *filename)
-//{
-//	printf("Opening %s for reading...\n", filename);
-//
-//	char currentChar = 0;
-//	FILE *handle;
-//
-//	// Open the file
-//	handle = fopen(filename, "r");
-//
-//	// If we didn't get a valid handle back, an error occurred
-//	if(handle == NULL)
-//	{
-//		perror("Error while opening the file.\n");
-//		return;
-//	}
-//
-//	// Read every character and print it to the screen
-//	while((currentChar = fgetc(handle)) != EOF)
-//	{
-//		printf("%c", currentChar);
-//	}
-//
-//	// Close the file cleanly
-//	fclose(handle);
-//
-//	return;
-//}
