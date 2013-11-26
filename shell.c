83c05d6253e0b3777e0b86217efa9613c9561f75
Scott Mann <ssmann@smu.edu>
2013-11-26 01:23:40 -0600

    Steps 5 and 6 work on first command, but not subsequent commands.

--- shell.c
+++ shell.c
@@ -3,10 +3,17 @@
 
 // Function Prototypes
 void displayError();
+int isType(char* input);
 
 // Main processing loop for the shell
 int main() {
 	char input[255];
+	char buffer[13312];
+	char part[1][255];
+	int i = 0;
+	int partIndex = 0;
+	int offset = 0;
+
 	interrupt(0x21, 0, "Shell Version 0.1.234\r\n\0", 0, 0);
 
 	// Begin the command processing loop
@@ -16,13 +23,79 @@ int main() {
 		interrupt(0x21, 0, "Shell> ", 0, 0);
 		interrupt(0x21, 1, input, 0, 0);
 		interrupt(0x21, 0, "\r\n\0", 0, 0);
-		displayError();
+
+		for (i = 0; i < 255; i++)
+		{
+			if (input[i] == ' ')
+			{
+				partIndex++;
+				offset = 0;
+				continue;
+			}
+			else if (input[i] == '\0')
+			{
+				break;
+			}
+
+			part[partIndex][offset] = input[i];
+			offset++;
+		}
+
+		if (matches("execute", part[0]) == 1)
+		{
+			interrupt(0x21, 0x6, part[1], 0, 0);
+		}
+		else if (matches("type", part[0]) == 1)
+		{
+			//interrupt(0x21, 0, "Type!\r\n\0", 0, 0);
+			interrupt(0x21, 0x3, part[1], buffer, 0);
+			interrupt(0x21, 0x0, buffer, 0, 0);
+		}
+		else
+		{
+			displayError();
+		}
 
 	}
 
 	return 0;
 }
 
+int matches(char* s1, char* s2)
+{
+	int match = 1;
+	int n = 0;
+	int size1 = sizeof(s1);
+	int size2 = sizeof(s2);
+	int maxSize = 0;
+
+	if (size2 >= size1)
+	{
+		maxSize = size2;
+	}
+	else
+	{
+		maxSize = size1;
+	}
+
+	interrupt(0x21, 0, s1, 0, 0);
+	interrupt(0x21, 0, "\r\n\0", 0, 0);
+	interrupt(0x21, 0, s2, 0, 0);
+	interrupt(0x21, 0, "\r\n\0", 0, 0);
+
+    //Compare the name
+    for (n = 0; n < maxSize; n++)
+    {
+        if(s1[n] != s2[n])
+        {
+        	interrupt(0x21, 0, "No match\r\n\0", 0, 0);
+            return 0;
+        }
+    }
+
+	interrupt(0x21, 0, "Match\r\n\0", 0, 0);
+    return 1;
+}
 
 // Display the erroneous command back to the user
 void displayError()
